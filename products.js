126f77a25308f468dbb15faa0cef851f7b1340a7
elahevakili1382 <elahevakili8@gmail.com>
2025-04-21 12:55:34 +0330

    Add files via upload

---
+++ products.js
@@ -0,0 +1,22 @@
+const products = [
+    {
+      id: 1,
+      name: "کلاه ایمنی مهندسی",
+      image: "./images/products/helmet.jpg",
+      description: "کلاه ایمنی سبک با مقاومت بالا، مناسب پروژه‌های ساختمانی و صنعتی."
+    },
+    {
+      id: 2,
+      name: "کفش ایمنی Steel Toe",
+      image: "./images/products/safety-shoes.jpg",
+      description: "کفش ایمنی با پنجه فولادی و زیره ضد لغزش، مناسب محیط‌های صنعتی."
+    },
+    {
+      id: 3,
+      name: "ماسک تنفسی FFP2",
+      image: "./images/products/mask.jpg",
+      description: "ماسک با فیلتر دوبل و کیفیت بالا، محافظت در برابر ذرات ریز و مواد شیمیایی."
+    }
+  ];
+
+  
